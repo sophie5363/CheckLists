475251db56c79b20fcd478f3854d35c838a289e4
Sophie <sophienanterre@gmail.com>
2021-02-15 12:46:10 +0100

    5)

--- CheckLists/ChecklistViewController.swift
+++ CheckLists/ChecklistViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath);
+        cell.textLabel!.text = "texte de test"
+        return cell
+    
+    
     }
 
 
