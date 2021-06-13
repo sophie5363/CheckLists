2b8641476f89457897bb0dfd89ee8323a37c5268
Sophie <sophienanterre@gmail.com>
2021-06-13 13:46:18 +0200

    28) désactivation du bouton Done si aucun texte n'a été tapé

--- Controllers/AddItemViewController.swift
+++ Controllers/AddItemViewController.swift
@@ -8,33 +8,49 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         tableViewCellText.becomeFirstResponder()
     }
     
+    @IBOutlet weak var doneButton: UIBarButtonItem!
     @IBOutlet weak var tableViewCellText: UITextField!
     
     @IBAction func textFieldWrittenText(_ sender: UITextField) {
         print(self.tableViewCellText.text!)
     }
     
-
-    @IBAction func didTapDoneButton(_ sender: UIBarButtonItem) {
-        func didEndOnExit(_ sender: UITextField) {
-        }
+    
+    @IBAction func done() {
+        
         print(tableViewCellText.text!)
         dismiss(animated: true, completion: nil)
     }
-    
-   
-    
-    
-    
-    
+
+}
+
+//extension AddItemViewController: UITextFieldDelegate {
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//        if let oldString = textField.text {
+//            let resultingString = oldString.replacingCharacters(in: Range(range, in: oldString)!, with: string)
+//            doneButton.isEnabled = resultingString.count > 0
+//        }
+//
+//        return true
+//    }
+//}
+
+extension AddItemViewController : UITextFieldDelegate{
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let newString = oldText.replacingCharacters(in: Range(range, in: oldText)!, with: string)
+        doneButton.isEnabled = newString.count > 0
+        return true
+    }
 }
